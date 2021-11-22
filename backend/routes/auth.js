71bff606a9d9d057e02950b71890b3f152a1b3d6
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2021-11-22 13:27:52 +0530

    Refractoring

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const User = require('../models/User');
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
 const fetchUser = require('../middleware/fetchuser')
 
 //Route1 : Create a User using: POST "/api/auth/createuser". Doesn't require Auth
 router.post('/createuser', [
     body('email', "Invalid Email Address").isEmail(),
     body('password', "Password Lenght Should be Greater than 5").isLength({ min: 5 }),
-    body('name', "Name lenght Should be atLeast 2 ").isLength({ min: 2 }),
+    body('name', "Name lenght Should be atLeast 2").isLength({ min: 2 })
 ], async (req, res) => {
     try {
         // Check for errors and if exist return Bad Request 
@@ -32,66 +32,55 @@ router.post('/createuser', [
                 email: req.body.email,
                 password: secPass
             })
-            const authToken = jwt.sign({ user: {id:user.id} }, "mySectretString")
+            const authToken = jwt.sign({ user: { id: user.id } }, "mySectretString")
             res.json({ authToken })
         }
     } catch (error) {
         console.error(error.Message)
-        res.status(500).send("Some Internal Error Ocuured!")
+        res.status(500).send("Some Internal Error Occured!")
     }
 })
 
 //Route2 : Login a User Using: POST "/api/auth/login".
 router.post('/login', [
     body('email', "Invalid Email Address").isEmail(),
-    body('password', "Password cannot be blank").exists()
+    body('password', "Password Lenght Should be Greater than 5").isLength({ min: 5 })
 ], async (req, res) => {
     try {
-    const {email,password} = req.body
-    let user = await User.findOne({email:email})
-    if(!user){
-        return res.status(400).json({error : "Invalid Credentials(email)"})
-    }else{
-        // Validating Password 
-        const matchPassword = bcrypt.compareSync(password,user.password)
-        if(matchPassword){
-            const authToken = jwt.sign({ user: {id:user.id} }, "mySectretString")
-            res.json({ authToken })
-        }else{
-            return res.status(400).json({error : "Invalid Credentials(Password)"})
+        const { email, password } = req.body
+        let user = await User.findOne({ email: email })
+        if (!user) {
+            return res.status(400).json({ error: "Invalid Credentials(email)" })
+        } else {
+            // Validating Password 
+            const matchPassword = bcrypt.compareSync(password, user.password)
+            if (matchPassword) {
+                const authToken = jwt.sign({ user: { id: user.id } }, "mySectretString")
+                res.json({ authToken })
+            } else if (!matchPassword) {
+                return res.status(400).json({ error: "Invalid Credentials(Password)" })
+            }
         }
-    }
     } catch (error) {
         console.error(error.Message)
         res.status(500).send("Some Internal Error Occured while Validating credentials!")
     }
-
 })
 
 //Route3 : Get LoggedIn user data: POST "/api/auth/getuser".
-router.post('/getuser',fetchUser, async (req, res) => {
+router.post('/getuser', fetchUser, async (req, res) => {
     try {
         const userID = req.user.id
         const userData = await User.findById(userID).select("-password")
-        res.send(userData)
+        if (!userData) {
+            return res.status(400).json({ error: "User not found with provided auth-token" })
+        } else if (userData) {
+            res.send(userData)
+        }
     } catch (error) {
         console.error(error.Message)
         res.status(500).send("Some Internal Error Ocuured!!")
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router
