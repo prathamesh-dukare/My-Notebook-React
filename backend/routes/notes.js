4ee2e84d9e017f68b725350affb23779e3cb2719
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2021-11-23 13:01:54 +0530

    Backend :: Created Update & Delete Endpoints

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -55,4 +55,70 @@ router.post('/createnote', fetchUser, [
     }
 })
 
+//Route3 : Update Notes: PUT "/api/notes/updatenote": Login Required!
+router.put('/updatenote/:id', fetchUser, [
+    body('title', "Title cannot be blank").isLength({ min: 1 }),
+    body('description', "Description cannot be blank").isLength({ min: 1 }),
+    body('tags', "Tags cannot be blank").isLength({ min: 1 })
+], async (req, res) => {
+    try {
+        const { title, description, tags } = req.body;
+        // Check for errors and if exist return Bad Request 
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        //Finding a note & using provided note ID
+        if(req.params.id.length !==24){
+            return res.status(400).send("Not Found!")
+        }
+        const currentNote = await Notes.findById(req.params.id)
+        if (!currentNote) {
+            return res.status(404).send("Not Found!")
+        } else {
+            if (currentNote.user.toString() !== req.user.id) {
+                return res.status(401).send("Not alllowed!")
+            } else {
+                const newNote = {}
+                if (title) { newNote.title = title }
+                if (description) { newNote.description = description }
+                if (tags) { newNote.tags = tags }
+                const updatedNote = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+                res.send(updatedNote)
+            }
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Some Internal Error Ocuured!")
+    }
+})
+
+//Route4 : Delete Notes: DELETE "/api/notes/deletenote": Login Required! 
+router.delete('/deletenote/:id', fetchUser, async (req, res) => {
+    try {
+         // Check for errors and if exist return Bad Request 
+         const errors = validationResult(req);
+         if (!errors.isEmpty()) {
+             return res.status(400).json({ errors: errors.array() })
+         }
+         //Finding a note & using provided note ID
+        if(req.params.id.length !==24){
+            return res.status(400).send("Not Found!")
+        }
+        const currentNote = await Notes.findById(req.params.id)
+        if (!currentNote) {
+            return res.status(404).send("Not Found!")
+        } else {
+            if (currentNote.user.toString() !== req.user.id) {
+                return res.status(401).send("Not alllowed!")
+            } else {
+                await Notes.findByIdAndDelete(req.params.id)
+                res.send({"Success":"Deleted Note Succesfully","id":`${req.params.id}`})
+            }          
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Some Internal Error Ocuured!")
+    }
+})
 module.exports = router
