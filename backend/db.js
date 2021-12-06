86be7b72d144706c65b2fa023545e05d8422b281
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2021-12-06 18:52:03 +0530

    Deleted : Backend Files

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const mongoURI = "mongodb://localhost:27017/my-notebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
-
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("connected to Mongo!")
-    })
-}
-module.exports = connectToMongo;
